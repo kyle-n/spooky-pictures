a93808f33e93e3952552c377b7bf723686fd12e3
Kyle Nazario <knazario@apple.com>
2024-08-18 16:43:06 -0600

    Adds horror lists

--- src/details/lists.ts
+++ src/details/lists.ts
@@ -1,6 +1,10 @@
 import type { HomepageList } from '$lib/types/homepage-list';
 
 export const homepageLists: HomepageList[] = [
-  { order: 0, tmdbId: 8310773, internalLabel: 'light' },
-  { order: 1, name: 'Legendary directors', tmdbIds: [11770] }
+  { tmdbId: 8310773, internalLabel: 'light' },
+  { name: 'Legendary directors', tmdbIds: [11770] },
+  { tmdbId: 8310775, internalLabel: 'comedies' },
+  { tmdbId: 8310776, internalLabel: 'classics' },
+  { tmdbId: 8310777, internalLabel: 'scary-silly' },
+  { tmdbId: 8310778, internalLabel: 'political' },
 ];
--- src/lib/types/homepage-list.ts
+++ src/lib/types/homepage-list.ts
@@ -1,13 +1,11 @@
 export type HomepageList = HomepageMovieList | HomepagePersonList;
 
 export type HomepageMovieList = {
-  order: number;
   internalLabel: string;
   tmdbId: number;
 };
 
 export type HomepagePersonList = {
-  order: number;
   name: string;
   tmdbIds: number[];
 };
