4a8e85307e1d426c425eef62a5a661218390a406
Kyle Nazario <knazario@apple.com>
2024-08-18 16:24:21 -0600

    Deletes Oscar bios

--- src/details/lists.ts
+++ src/details/lists.ts
@@ -2,5 +2,5 @@ import type { HomepageList } from '$lib/types/homepage-list';
 
 export const homepageLists: HomepageList[] = [
   { order: 0, tmdbId: 8310773, internalLabel: 'light' },
-  { order: 1, name: 'Best Actor', tmdbIds: [51329, 91671, 13242, 2037, 2954] }
+  { order: 1, name: 'Legendary directors', tmdbIds: [11770] }
 ];
