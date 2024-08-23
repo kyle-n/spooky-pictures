09b7183f3b7b1c8b5ccc73450f9778a701856889
Kyle Nazario <knazario@apple.com>
2024-08-23 13:44:25 -0600

    ED2 / SR reviews

--- src/details/lists.ts
+++ src/details/lists.ts
@@ -2,7 +2,10 @@ import type { HomepageList } from '$lib/types/homepage-list';
 
 export const homepageLists: HomepageList[] = [
   { tmdbId: 8310773, internalLabel: 'light' },
-  { name: 'Great directors', tmdbIds: [11770, 291263, 5140, 2127, 551463] },
+  {
+    name: 'Great directors',
+    tmdbIds: [11770, 7623, 54025, 291263, 5140, 2127, 551463]
+  },
   { tmdbId: 8310775, internalLabel: 'comedies' },
   { tmdbId: 8310776, internalLabel: 'classics' },
   { name: 'Final girls', tmdbIds: [10205] },
