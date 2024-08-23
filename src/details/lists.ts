128e6f250575d8b28d69e51730453bfdb3b7f96e
Kyle Nazario <knazario@apple.com>
2024-08-23 11:45:29 -0600

    Fills out director list

--- src/details/lists.ts
+++ src/details/lists.ts
@@ -2,7 +2,7 @@ import type { HomepageList } from '$lib/types/homepage-list';
 
 export const homepageLists: HomepageList[] = [
   { tmdbId: 8310773, internalLabel: 'light' },
-  { name: 'Legendary directors', tmdbIds: [11770] },
+  { name: 'Great directors', tmdbIds: [11770, 291263, 5140, 2127, 551463] },
   { tmdbId: 8310775, internalLabel: 'comedies' },
   { tmdbId: 8310776, internalLabel: 'classics' },
   { name: 'Final girls', tmdbIds: [10205] },
