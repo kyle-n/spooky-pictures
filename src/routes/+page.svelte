03d9cedd631f5509636b40de5ed6dd6de6e1f6cd
Kyle Nazario <knazario@apple.com>
2024-09-13 14:28:10 -0600

    Unlocks movies client-side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,31 @@
   import type { ResolvedCalendarData } from '$lib/types/resolved-data';
   import PageTitle from '$lib/components/page-title.svelte';
   import MoviePoster from '$lib/components/movie-poster.svelte';
+  import { onMount } from 'svelte';
+  import type { TMDBMovie } from '$lib/types/tmdb-movie';
 
   const { data } = $props<{ data: ResolvedCalendarData }>();
+  let unlockedMovies = $state<TMDBMovie[]>([]);
+
+  onMount(() => {
+    const dayOfMonth = new Date().getDate();
+    unlockedMovies = data.list.items.slice(0, dayOfMonth);
+  });
 </script>
 
 <PageTitle content="Calendar" />
 
 <div id="calendar">
   <div id="calendar-list">
-    {#each data.list.items as movie, i}
-    <div>
-      <div class="date-number">
-      <h3>{i + 1}</h3>
+    {#each unlockedMovies as movie, i}
+      <div>
+        <div class="date-number">
+          <h3>{i + 1}</h3>
+        </div>
+        <div class="calendar-day">
+          <MoviePoster {movie} />
+        </div>
       </div>
-      <div class="calendar-day">
-        <MoviePoster {movie} />
-      </div>
-    </div>
     {/each}
   </div>
 </div>
