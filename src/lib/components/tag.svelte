9124f53ddf3a487df99aa199f17a2e8e091f73fc
Kyle Nazario <knazario@apple.com>
2024-09-18 23:16:03 -0600

    Rainbow tag

--- src/lib/components/tag.svelte
+++ src/lib/components/tag.svelte
@@ -67,14 +67,18 @@
       MovieTag.Comedy,
       MovieTag.Romance,
       MovieTag.Scifi,
-      MovieTag.Artsy
+      MovieTag.Artsy,
+      MovieTag.Queer
     ].includes(tag)
   ) {
     textColor = 'black';
   }
 </script>
 
-<div style="--backgroundColor: {tagColors[tag]}; --textColor: {textColor}">
+<div
+  style="--backgroundColor: {tagColors[tag]}; --textColor: {textColor}"
+  class:rainbow={tag === MovieTag.Queer}
+>
   {tagNames[tag]}
 </div>
 
@@ -86,5 +90,22 @@
     margin: 0.5rem;
     color: var(--textColor);
     mix-blend-mode: difference;
+  }
+
+  .rainbow {
+    background: linear-gradient(
+      90deg,
+      rgba(255, 0, 0, 1) 0%,
+      rgba(255, 154, 0, 1) 10%,
+      rgba(208, 222, 33, 1) 20%,
+      rgba(79, 220, 74, 1) 30%,
+      rgba(63, 218, 216, 1) 40%,
+      rgba(47, 201, 226, 1) 50%,
+      rgba(28, 127, 238, 1) 60%,
+      rgba(95, 21, 242, 1) 70%,
+      rgba(186, 12, 248, 1) 80%,
+      rgba(251, 7, 217, 1) 90%,
+      rgba(255, 0, 0, 1) 100%
+    );
   }
 </style>
