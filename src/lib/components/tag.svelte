621bd781ba46e3e0ebc953b60cd8a7faa50e19d3
Kyle Nazario <knazario@apple.com>
2024-09-18 23:14:30 -0600

    Tag colors

--- src/lib/components/tag.svelte
+++ src/lib/components/tag.svelte
@@ -33,17 +33,58 @@
     [MovieTag.Musical]: 'Musical',
     [MovieTag.Cosmic]: 'Cosmic Horror'
   };
+
+  const tagColors: Record<MovieTag, string> = {
+    [MovieTag.Comedy]: 'yellow',
+    [MovieTag.Slasher]: 'red',
+    [MovieTag.PG13]: 'blue',
+    [MovieTag.Zombie]: 'green',
+    [MovieTag.Romance]: 'pink',
+    [MovieTag.Women]: 'purple',
+    [MovieTag.Gory]: 'black',
+    [MovieTag.Queer]: 'rainbow',
+    [MovieTag.Scifi]: 'silver',
+    [MovieTag.Classic]: 'gold',
+    [MovieTag.Monster]: 'brown',
+    [MovieTag.ExtraScary]: 'darkred',
+    [MovieTag.SillyScary]: 'orange',
+    [MovieTag.Trauma]: 'darkgrey',
+    [MovieTag.Kids]: 'lightblue',
+    [MovieTag.Artsy]: 'lightgrey',
+    [MovieTag.Family]: 'lightgreen',
+    [MovieTag.International]: 'lightyellow',
+    [MovieTag.Vampire]: 'darkpurple',
+    [MovieTag.ZeroScares]: 'white',
+    [MovieTag.Political]: 'darkblue',
+    [MovieTag.PoC]: 'darkgreen',
+    [MovieTag.Musical]: 'darkpink',
+    [MovieTag.Cosmic]: 'darkgold'
+  };
+
+  let textColor = 'white';
+  if (
+    [
+      MovieTag.Comedy,
+      MovieTag.Romance,
+      MovieTag.Scifi,
+      MovieTag.Artsy
+    ].includes(tag)
+  ) {
+    textColor = 'black';
+  }
 </script>
 
-<div>
+<div style="--backgroundColor: {tagColors[tag]}; --textColor: {textColor}">
   {tagNames[tag]}
 </div>
 
 <style lang="scss">
   div {
-    background-color: yellowgreen;
+    background-color: var(--backgroundColor);
     padding: 0.5rem;
     border-radius: 0.5rem;
     margin: 0.5rem;
+    color: var(--textColor);
+    mix-blend-mode: difference;
   }
 </style>
